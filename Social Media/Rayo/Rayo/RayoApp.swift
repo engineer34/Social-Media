09b8efce4ff2b5ece6773bbd937f82ea041d2c78
Javier Medina <130412345+engineer34@users.noreply.github.com>
2025-09-13 14:47:11 -0500

    Add files via upload

---
+++ Social Media/Rayo/Rayo/RayoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayoApp.swift
+//  Rayo
+//
+//  Created by Feliciano Medina on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RayoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
