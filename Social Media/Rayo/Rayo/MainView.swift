299219de2e0b42297bad7cb8570f70da3a5b5c8e
Javier Medina <130412345+engineer34@users.noreply.github.com>
2025-09-17 14:15:12 -0500

    Add files via upload

---
+++ Social Media/Rayo/Rayo/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Rayo
+//
+//  Created by Feliciano Medina on 9/17/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
